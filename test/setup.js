ef99e3a0e395de91cd89ebea9675e89eb7e15e7b
John Byrne <jbyrne87@gmail.com>
2021-01-24 22:39:14 -0600

    fixed DB connection issue

--- test/setup.js
+++ test/setup.js
@@ -5,8 +5,9 @@ process.env.JWT_EXPIRY = '3m'
 
 require('dotenv').config()
 
-process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://dunder-mifflin@localhost/spaced-repetition-test"
+process.env.DB_URL =
+  process.env.DB_URL ||
+  'postgresql://dunder_mifflin@localhost/spaced-repetition'
 
 const { expect } = require('chai')
 const supertest = require('supertest')
