c91923aba568b28602c5ab8df6c9c617fe662fe4
John Byrne <jbyrne87@gmail.com>
2021-01-27 22:55:58 -0600

    updated credentials

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    'postgresql://dunder_mifflin@localhost/spaced-repetition',
+    'postgres://moxyqqbxmvsjzq:c93b3583f214a6b039ca11aa5cefccd4b5c5fc4e2047682a90a076ccad02a32c@ec2-18-214-119-135.compute-1.amazonaws.com:5432/d7hmbhse1il5tt',
   JWT_SECRET: process.env.JWT_SECRET || 'dji1312701',
 }
