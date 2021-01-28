9bc165f9049ab823d5f867a8cfda2af10c53755a
John Byrne <jbyrne87@gmail.com>
2021-01-27 22:27:44 -0600

    edited database links

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
-    || 'postgresql://dunder_mifflin@localhost/spaced-repetition',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    'postgresql://dunder_mifflin@localhost/spaced-repetition',
+  JWT_SECRET: process.env.JWT_SECRET || 'dji1312701',
 }
--- test/setup.js
+++ test/setup.js
@@ -4,8 +4,8 @@ process.env.JWT_SECRET = 'test-jwt-secret'
 
 require('dotenv').config()
 
-process.env.DB_URL =
-  process.env.DB_URL ||
+process.env.DATABASE_URL =
+  process.env.DATABASE_URL ||
   'postgresql://dunder_mifflin@localhost/spaced-repetition'
 
 const { expect } = require('chai')
